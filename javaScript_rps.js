a8fbdbd826f4ff116b3bfda17dbb157bdec04c9c
“Zaibatus” <paolosalvetti34@gmail.com>
2023-12-15 09:22:05 +0100

    Add playRound founction

--- javaScript_rps.js
+++ javaScript_rps.js
@@ -12,13 +12,25 @@ function getComputerChoice() {
     }
 }
 
-function singleRound (playerSelection, computerSelection) {
-
-    playerSelection.toUpperCase;
+function playRound (playerSelection, computerSelection) {
 
     if (playerSelection === 'ROCK' && computerSelection === 'PAPER') {
-        return "You Lose! Paper beats Rock"; 
+        return "You Lose! Paper beats Rock,"; 
+        } else if (playerSelection === 'ROCK' && computerSelection === 'SCISSORS') {
+            return "You win! Rock beats Scissors."; 
+        } else if (playerSelection === 'SCISSORS' && computerSelection === 'PAPER') {
+            return "You win! Scissors beats Paper."; 
+        } else if (playerSelection === 'SCISSORS' && computerSelection === 'ROCK') {
+            return "You Lose! Rock beats Scissors.";
+        } else if (playerSelection === 'PAPER' && computerSelection === 'ROCK') {
+            return "You win! Paper beats Rock.";
+        } else if (playerSelection === 'PAPER' && computerSelection === 'SCISSORS') {
+            return "You Lose! Scissors beats Paper.";
+        } else {
+            return "It's a TIE!";
+        }   
     }
-}
 
-console.log(singleRound("ROCk","PAPER"));
+const playerSelection = 'ROCK';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection,computerSelection));
